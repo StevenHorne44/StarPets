d4a06cf8b7be4dd46f160e609007c9ce033dad8c
Steven Horne <3005574H@student.gla.ac.uk>
2026-03-02 21:34:30 +0000

    Made all basic pages and navigation

--- pets/urls.py
+++ pets/urls.py
@@ -6,4 +6,10 @@ app_name = 'pets'
 urlpatterns = [
     # name='home' allows you to link to this page easily in your HTML
     path('', views.home, name='home'),
+    path('signup', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('rated/', views.top_pets, name='top_pets'),
+    path('browse/', views.categories, name='categories'),
+    path('bookmark/', views.bookmarks, name='bookmarks'),
+    path('upload/', views.upload_pets, name='upload'),
 ]
--- pets/views.py
+++ pets/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def home(request):
     return render(request, 'pets/home.html')
+
+#@login_required
+def top_pets(request):
+    return render(request, 'pets/top_pets.html')
+
+#@login_required
+def categories(request):
+    return render(request, 'pets/categories.html')
+
+#@login_required
+def bookmarks(request):
+    return render(request, 'pets/bookmarks.html')
+
+#@login_required
+def upload_pets(request):
+    return render(request, 'pets/upload.html')
+
+def register(request):
+    return render(request, 'pets/register.html')
+
+def login(request):
+    return render(request, 'pets/login.html')
