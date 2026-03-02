1082c5351ba2f2440cf96baad72d4ac7eebfeb8e
Steven Horne <3005574H@student.gla.ac.uk>
2026-03-02 16:57:20 +0000

    Synced models

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Pet(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    picture = models.ImageField(upload_to='pet_images', blank=True)
+    description = models.TextField(default="No description provided.")
+
+    def __str__(self):
+        return self.name
