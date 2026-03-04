43f87656b2fc9b9284b74d3b4cf17b25d2f16b94
Steven Horne <3005574H@student.gla.ac.uk>
2026-03-04 15:09:03 +0000

    Modification to admin/database

--- pets/admin.py
+++ pets/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from pets.models import Bookmark, PetType, Pet, PetRating
+from pets.models import Bookmark, PetType, Pet, PetRating, UserProfile
 
 admin.site.register(PetType)
 admin.site.register(Pet)
 admin.site.register(PetRating)  
 admin.site.register(Bookmark)
+admin.site.register(UserProfile)
