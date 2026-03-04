f61321da7d59c94613caa96d8140117cd77e2411
Steven Horne <3005574H@student.gla.ac.uk>
2026-03-04 14:43:37 +0000

    Some tests

--- pets/tests.py
+++ pets/tests.py
@@ -1,3 +1,15 @@
+from urllib import response
+
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class HomePageTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_correct_content(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Welcome to StarPets!')
