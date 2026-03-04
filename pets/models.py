f5bbb66ccf2cb0e8d1b32c69b39d7f5d727d55d6
Steven Horne <3005574H@student.gla.ac.uk>
2026-03-04 14:56:42 +0000

    Added user model

--- pets/models.py
+++ pets/models.py
@@ -45,3 +45,10 @@ class Bookmark(models.Model):
     def __str__(self):
         return f"{self.UserID.username} bookmarked {self.PetID.name}"
     
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+    
+
+    def __str__(self):
+        return self.user.username
