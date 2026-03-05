cbbf866627b1ff0e0e6c5c92e56053d93fa868f9
Steven Horne <3005574H@student.gla.ac.uk>
2026-03-05 13:39:49 +0000

    Navigation bar feature

--- pets/views.py
+++ pets/views.py
@@ -10,19 +10,19 @@ from .forms import ExtendedUserCreationForm
 def home(request):
     return render(request, 'pets/home.html')
 
-#@login_required
+@login_required
 def top_pets(request):
     return render(request, 'pets/top_pets.html')
 
-#@login_required
+@login_required
 def categories(request):
     return render(request, 'pets/categories.html')
 
-#@login_required
+@login_required
 def bookmarks(request):
     return render(request, 'pets/bookmarks.html')
 
-#@login_required
+@login_required
 def upload_pets(request):
     return render(request, 'pets/upload.html')
 
