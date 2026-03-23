7cb2c927a90f35b055489e0331d2f941718451cb
Steven Horne <3005574H@student.gla.ac.uk>
2026-03-23 20:29:40 +0000

    Added comments to pets

--- population_script.py
+++ population_script.py
@@ -4,7 +4,7 @@ import json
 import django
 django.setup()
 from django.contrib.auth.models import User 
-from pets.models import PetType, Pet, PetRating, UserProfile
+from pets.models import PetType, Pet, PetRating, UserProfile, Comment
 import shutil
 
 def populate():
@@ -53,18 +53,30 @@ def populate():
     user1 = add_user("Steven", "steven@example.com", "12345678f", "Steven.jpg")
     user2 = add_user("Alexander", "alexander@example.com", "123456789f", "Alexander.jpg")
     user3 = add_user("Bob", "bob@example.com", "12345678f", "Bob.jpg")
-    user4 = add_user("Marjorie", "marjoire@example.com", "12345678f", "Marjorie.png")
+    user4 = add_user("Marjorie", "marjorie@example.com", "12345678f", "Marjorie.png")
 
     # Manually defined pets with correct types
-    add_pet(dog_type,   user1, "Buddy",   "A happy german shepherd", 5, "PetPhotos/Buddy.jpg")
-    add_pet(cat_type,   user2, "Fluffy",  "A fluffy grey cat.",          4, "PetPhotos/Fluffy.jpg")
-    add_pet(fish_type,  user1, "Nemo",    "A bright orange clownfish.",   5, "PetPhotos/Nemo.jpg")
-    add_pet(rabbit_type,user3, "Bunny",   "A soft brown rabbit.",         4, "PetPhotos/Bunny.jpg")
-    add_pet(dog_type,   user2, "Gold",    "A friendly golden retriever.", 3, "PetPhotos/Gold.jpg")
-    add_pet(parrot_type,   user4, "Skittles","A colourful playful parrot.",       5, "PetPhotos/Skittles.jpg")
-    add_pet(snake_type,   user3, "Noodle",  "A long snake.",            4, "PetPhotos/Noodle.jpg")
-    add_pet(turtle_type,   user4, "Speedy",  "A slow cheerful turtle.",              5, "PetPhotos/Speedy.jpg")
+    buddy = add_pet(dog_type,   user1, "Buddy",   "A happy german shepherd", 5, "PetPhotos/Buddy.jpg")
+    fluffy = add_pet(cat_type,   user2, "Fluffy",  "A fluffy grey cat.",          4, "PetPhotos/Fluffy.jpg")
+    nemo = add_pet(fish_type,  user1, "Nemo",    "A bright orange clownfish.",   5, "PetPhotos/Nemo.jpg")
+    bunny = add_pet(rabbit_type,user3, "Bunny",   "A soft brown rabbit.",         4, "PetPhotos/Bunny.jpg")
+    gold = add_pet(dog_type,   user2, "Gold",    "A friendly golden retriever.", 3, "PetPhotos/Gold.jpg")
+    skittles = add_pet(parrot_type,   user4, "Skittles","A colourful playful parrot.",       5, "PetPhotos/Skittles.jpg")
+    noodle = add_pet(snake_type,   user3, "Noodle",  "A long snake.",            4, "PetPhotos/Noodle.jpg")
+    speedy = add_pet(turtle_type,   user4, "Speedy",  "A slow cheerful turtle.",              5, "PetPhotos/Speedy.jpg")
 
+    #Comments
+    add_comment(buddy, user2, "What a cute dog!")
+    add_comment(buddy, user4, "So cute!")
+    add_comment(buddy, user3, "He's so happy!")
+    add_comment(fluffy, user1, "Wow!")
+    add_comment(skittles, user3, "Colourful wow!")
+    add_comment(speedy,   user1, "Love the ironic name!")
+    add_comment(speedy,   user2, "Turtles are cool")
+    add_comment(noodle, user2, "Brilliant name")
+    add_comment(gold, user1, "I want one")
+    add_comment(bunny, user4, "Adorable")
+    add_comment(nemo, user3, "GOOD NAME!")
 
     print("Database population complete.")
 
@@ -91,6 +103,12 @@ def add_pet(pet_type, user, name, description, stars, photo_path):
 
     PetRating.objects.get_or_create(PetID=pet, UserID=user, stars=stars)
     return pet
+
+def add_comment(pet, user, content):
+    comment, created = Comment.objects.get_or_create(PetID=pet, UserID=user)
+    comment.content = content
+    comment.save()
+    return comment
 
 
 if __name__ == '__main__':
